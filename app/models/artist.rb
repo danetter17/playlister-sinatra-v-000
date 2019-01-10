c62e61ff86297d0740d53e4d563691479962d381
Daniel Etter <etter.dt@gmail.com>
2019-01-10 04:15:20 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,13 +5,6 @@ class Artist < ActiveRecord::Base
 
   def self.find_by_slug(slug)
     Artist.all.find {|artist| artist.slug == slug}
-    # unslugged = slug.split("-")
-    # self.all.map do |artist|
-    #   if artist.name.downcase.include?(unslugged[0]) && artist.name.downcase.include?(unslugged[1])
-    #     unslugged = artist
-    #   end
-    # end
-    # self.find_by_name(unslugged.name)
   end
 
   has_many :songs
